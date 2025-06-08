983a5adca8cea43c6ab8d45a51fb5fc25c47ad25
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-06-08 14:42:48 +0300

    added / to beggining of path

--- src/startup/routes.ts
+++ src/startup/routes.ts
@@ -6,5 +6,5 @@ import logInsRouter from "../routes/logIns.js";
 export default function routes(app: Application): void {
   app.use("/api/signUps", signUpRoute);
   app.use("/api/locations", locationsRouter);
-  app.use("api/logIns", logInsRouter);
+  app.use("/api/logIns", logInsRouter);
 }
