618f6756194635ec74ba41e4b09001fddb11c8df
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-06-08 14:18:04 +0300

    added constraints to logIns validators

--- src/validators/logIns.validators.ts
+++ src/validators/logIns.validators.ts
@@ -1,8 +1,8 @@
 import Joi from "joi";
 
 const postSchema = Joi.object({
-  password: Joi.string(),
-  email: Joi.string(),
+  password: Joi.string().min(8).max(25),
+  email: Joi.string().email(),
 }).or("password", "email");
 
 export default function (data: { email?: string; password?: string }) {
