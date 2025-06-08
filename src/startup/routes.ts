ec7c07fa77134b4b01094af6996c630f29218af5
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-06-08 14:14:51 +0300

    set up logIns router

--- src/startup/routes.ts
+++ src/startup/routes.ts
@@ -1,7 +1,10 @@
 import { Application } from "express";
 import signUpRoute from "../routes/signUps.js";
 import locationsRouter from "../routes/locations.js";
+import logInsRouter from "../routes/logIns.js";
+
 export default function routes(app: Application): void {
   app.use("/api/signUps", signUpRoute);
   app.use("/api/locations", locationsRouter);
+  app.use("api/logIns", logInsRouter);
 }
