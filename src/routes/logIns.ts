dc873f7a5cb6db13e4304c5ad89a74eef691fe9c
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-06-08 14:57:14 +0300

    added logIns route. tested. working

--- src/routes/logIns.ts
+++ src/routes/logIns.ts
@@ -1,7 +1,53 @@
 import express, { Request, Response } from "express";
+import logInsValidators from "../validators/logIns.validators.js";
+import { prisma } from "../utils/prismaClient.js";
+import { unHash } from "../utils/hashPassword.js";
+import { doesEmailMatchHash } from "../utils/hashEmail.js";
 
 const router = express.Router();
 
-router.post("/", async (req: Request, res: Response) => {});
+router.post("/", async (req: Request, res: Response) => {
+  const data = req.body;
+  const result = logInsValidators(data);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+  const userId: string = data.userId;
+  try {
+    const user = await prisma.users.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+    if (!user) {
+      res.status(404).send(`user with id ${userId} not found`);
+      return;
+    }
+    if (data.password) {
+      const isPasswordValid = await unHash(user.password_hash, data.password);
+      if (!isPasswordValid) {
+        res.status(403).send("password incorrect");
+        return;
+      }
+      //send jwt here
+      res.status(200).send("password correct");
+    }
+    if (data.email) {
+      console.log("being called");
+
+      const isEmailCorrect = doesEmailMatchHash(data.email, user.email_hash);
+      if (!isEmailCorrect) {
+        res.status(403).send("email incorrect");
+        return;
+      }
+      //add email validation logic. send 1tp to users email, save in db for 5 min. forward user to login page via link in email.
+      res.status(200).send("email correct");
+    }
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).send("internal server error");
+  }
+});
 
 export default router;
