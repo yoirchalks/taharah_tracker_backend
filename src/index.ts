67c2a819d51b43158630a5c799a5496a6ad6dc0d
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-06-09 14:58:01 +0300

    added cookie-parser

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import routes from "./startup/routes.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 
 routes(app);
