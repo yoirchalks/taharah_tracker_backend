f2828f9639c2d2fb0f075e7ff184948f331dde66
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-06-10 06:18:27 +0300

    added OTP to validator

--- src/validators/logIns.validators.ts
+++ src/validators/logIns.validators.ts
@@ -4,6 +4,7 @@ const postSchema = Joi.object({
   userId: Joi.string().min(36).max(36).required(),
   password: Joi.string().min(8).max(25),
   email: Joi.string().email(),
+  otp: Joi.string().min(6).max(6),
 }).or("password", "email");
 
 export default function (data: { email?: string; password?: string }) {
