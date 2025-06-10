c9872e1560189b40145e459023f58c930f847769
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-06-10 09:36:54 +0300

    added otp logic. tested. working

--- src/routes/logIns.ts
+++ src/routes/logIns.ts
@@ -51,6 +51,7 @@ router.post("/", async (req: Request, res: Response) => {
         .status(204)
         .send();
     }
+
     if (data.email) {
       const isEmailCorrect = doesEmailMatchHash(data.email, user.email_hash);
       if (!isEmailCorrect) {
@@ -60,7 +61,7 @@ router.post("/", async (req: Request, res: Response) => {
       //TODO:forward user to front-end login page via link in email.
 
       const Otp = createOtp();
-      prisma.oneTimePasswords.create({
+      await prisma.otp.create({
         data: {
           userId: userId,
           OTP: Otp,
@@ -68,6 +69,45 @@ router.post("/", async (req: Request, res: Response) => {
       });
       sendEmail(data.email, Otp);
       res.status(200).send("email correct");
+    }
+
+    if (data.otp) {
+      const storedOtp = await prisma.otp.findFirst({
+        where: {
+          userId: userId,
+        },
+        orderBy: {
+          iat: "desc",
+        },
+      });
+
+      if (!storedOtp) {
+        res.status(404).send(`no OTP found for user with id ${userId}`);
+        return;
+      }
+
+      const elapsedTime = new Date().getTime() - storedOtp.iat.getTime();
+      console.log("elapsed time: ", elapsedTime);
+
+      if (elapsedTime > 1000 * 60 * 10) {
+        res.status(401).send("OTP expired");
+        return;
+      }
+
+      if (storedOtp.OTP != data.otp) {
+        res.status(401).send("OTP incorrect");
+        return;
+      }
+      const jwt = signJwt({ userId });
+      res
+        .cookie("authentication", jwt, {
+          secure: true,
+          sameSite: "strict",
+          path: "/",
+          httpOnly: true,
+        })
+        .status(204)
+        .send();
     }
   } catch (error) {
     console.log("error", error);
