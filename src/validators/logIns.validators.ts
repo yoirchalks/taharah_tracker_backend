08ded46743ed6794d14dac50169a02fe95b52986
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-06-15 12:38:41 +0300

    updated validator logic for use w/ updated otp

--- src/validators/logIns.validators.ts
+++ src/validators/logIns.validators.ts
@@ -13,6 +13,8 @@ const schema = Joi.object({
 const otpSchema = Joi.object({
   UUID: Joi.string().required(),
   OTP: Joi.string().min(6).max(6),
+  IAT: Joi.string().required(),
+  used: Joi.boolean().required(),
 });
 
 export default function (data: any, isOtp: boolean) {
