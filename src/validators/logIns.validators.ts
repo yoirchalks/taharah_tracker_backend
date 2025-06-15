54044413b5424203e70989bfdf6bcd6989d49208
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-06-15 12:52:32 +0300

    seperated validators for otp and logIns

--- src/validators/logIns.validators.ts
+++ src/validators/logIns.validators.ts
@@ -10,13 +10,6 @@ const schema = Joi.object({
   }),
 });
 
-const otpSchema = Joi.object({
-  UUID: Joi.string().required(),
-  OTP: Joi.string().min(6).max(6),
-  IAT: Joi.string().required(),
-  used: Joi.boolean().required(),
-});
-
 export default function (data: any, isOtp: boolean) {
-  return isOtp ? otpSchema.validate(data) : schema.validate(data);
+  return schema.validate(data);
 }
---
+++ src/validators/otp.validators.ts
@@ -0,0 +1,12 @@
+import Joi from "joi";
+
+const otpSchema = Joi.object({
+  UUID: Joi.string().required(),
+  OTP: Joi.string().min(6).max(6),
+  IAT: Joi.string().required(),
+  used: Joi.boolean().required(),
+});
+
+export default function (data: any) {
+  return otpSchema.validate(data);
+}
