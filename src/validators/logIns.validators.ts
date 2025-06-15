4c7dbd131e2a6d8e5fd53d2cde69bc44b65f5460
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-06-15 04:21:35 +0300

    made email compulsory as it's only UID before user is known

--- src/validators/logIns.validators.ts
+++ src/validators/logIns.validators.ts
@@ -1,11 +1,10 @@
 import Joi from "joi";
 
 const postSchema = Joi.object({
-  userId: Joi.string().min(36).max(36).required(),
   password: Joi.string().min(8).max(25),
-  email: Joi.string().email(),
+  email: Joi.string().email().required(),
   otp: Joi.string().min(6).max(6),
-}).xor("password", "email", "otp");
+}).xor("password", "otp");
 
 export default function (data: { email?: string; password?: string }) {
   return postSchema.validate(data);
