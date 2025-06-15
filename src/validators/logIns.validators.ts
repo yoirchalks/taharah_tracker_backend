4e2dcc8f5f653750c37b1945bf33e89088e55714
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-06-15 04:27:56 +0300

    added requestingOtp to validator

--- src/validators/logIns.validators.ts
+++ src/validators/logIns.validators.ts
@@ -3,8 +3,9 @@ import Joi from "joi";
 const postSchema = Joi.object({
   password: Joi.string().min(8).max(25),
   email: Joi.string().email().required(),
+  requestingOtp: Joi.boolean(),
   otp: Joi.string().min(6).max(6),
-}).xor("password", "otp");
+}).xor("password", "otp", "requestingOtp");
 
 export default function (data: { email?: string; password?: string }) {
   return postSchema.validate(data);
