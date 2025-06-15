ef6e5c5046d9a3da3b933b5c890aec2ba5b298af
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-06-15 12:55:25 +0300

    registered otp router

--- src/startup/routes.ts
+++ src/startup/routes.ts
@@ -2,9 +2,11 @@ import { Application } from "express";
 import signUpRoute from "../routes/signUps.js";
 import locationsRouter from "../routes/locations.js";
 import logInsRouter from "../routes/logIns.js";
+import otpRouter from "../routes/otp.js";
 
 export default function routes(app: Application): void {
   app.use("/api/signUps", signUpRoute);
   app.use("/api/locations", locationsRouter);
   app.use("/api/logIns", logInsRouter);
+  app.use("api/otp", otpRouter);
 }
