e4c61a8836e91a824485529b7ae0af143d6abd70
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-06-17 22:17:56 +0300

    post method added. tested. working

--- src/routes/options.ts
+++ src/routes/options.ts
@@ -2,13 +2,29 @@ import authMiddleware from "../middlewares/jwt.middleware.js";
 import express from "express";
 import type { Request, Response } from "express";
 import { prisma } from "../utils/prismaClient.js";
-import { send } from "process";
+import validator from "../validators/options.validators.js";
 
 const router = express.Router();
 
 router.get("/", authMiddleware, async (req: Request, res: Response) => {});
 
-router.post("/", authMiddleware, async (req: Request, res: Response) => {});
+router.post("/", authMiddleware, async (req: Request, res: Response) => {
+  const userId = req.userId;
+  const result = validator(req.body, "post");
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+  const { location, receiveAlerts } = req.body;
+  const options = await prisma.options.create({
+    data: {
+      userId: userId,
+      location,
+      receiveAlerts,
+    },
+  });
+  res.send(options);
+});
 
 router.put("/:id", authMiddleware, async (req: Request, res: Response) => {});
 
