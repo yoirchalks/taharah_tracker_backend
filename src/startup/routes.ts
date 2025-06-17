d3ad7d6d02ce16ae2e2ee4ae5091d306967c7a4c
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-06-17 21:03:22 +0300

    registered options route

--- src/startup/routes.ts
+++ src/startup/routes.ts
@@ -1,10 +1,11 @@
 import type { Application } from "express";
 
-import signUpRoute from "../routes/signUps.js";
 import locationsRouter from "../routes/locations.js";
 import logInsRouter from "../routes/logIns.js";
+import optionsRouter from "../routes/options.js";
 import otpRouter from "../routes/otp.js";
 import periodsRouter from "../routes/periods.js";
+import signUpRoute from "../routes/signUps.js";
 
 export default function routes(app: Application): void {
   app.use("/api/signUps", signUpRoute);
@@ -12,4 +13,5 @@ export default function routes(app: Application): void {
   app.use("/api/logIns", logInsRouter);
   app.use("/api/otp", otpRouter);
   app.use("/api/periods", periodsRouter);
+  app.use("api/options", optionsRouter);
 }
