85794555fc8a7b980a4948803c4abde3b9c92f05
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-06-17 09:28:35 +0300

    added periods route

--- src/startup/routes.ts
+++ src/startup/routes.ts
@@ -4,10 +4,12 @@ import signUpRoute from "../routes/signUps.js";
 import locationsRouter from "../routes/locations.js";
 import logInsRouter from "../routes/logIns.js";
 import otpRouter from "../routes/otp.js";
+import periodsRouter from "../routes/periods.js";
 
 export default function routes(app: Application): void {
   app.use("/api/signUps", signUpRoute);
   app.use("/api/locations", locationsRouter);
   app.use("/api/logIns", logInsRouter);
   app.use("/api/otp", otpRouter);
+  app.use("/api/periods", periodsRouter);
 }
