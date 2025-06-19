d5668400ac3bf208d913e70aa4fd7a06a7fa0e82
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-06-19 22:50:31 +0300

    sqlite db seems to be working

---
+++ src/startup/geoDb.ts
@@ -0,0 +1,28 @@
+import { GeoDb } from "@hebcal/geo-sqlite";
+
+let locationDb: any;
+
+export function initGeoDb() {
+  locationDb = new GeoDb(console, "zips.sqlite3", "geonames.sqlite3");
+
+  process.on("SIGINT", () => {
+    cleanup();
+  });
+
+  process.on("SIGTERM", () => {
+    cleanup();
+  });
+
+  process.on("beforeExit", () => {
+    locationDb.close();
+  });
+}
+
+function cleanup() {
+  if (locationDb) {
+    locationDb.close();
+    console.log("Geo DB closed.");
+  }
+}
+
+export default locationDb;
