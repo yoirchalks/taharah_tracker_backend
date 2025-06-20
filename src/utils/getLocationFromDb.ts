8758373e640719fd9061b8febdfe8e0619b9757c
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-06-20 12:34:28 +0300

    assure db is only referenced after init in index.ts

--- src/utils/getLocationFromDb.ts
+++ src/utils/getLocationFromDb.ts
@@ -1,5 +1,7 @@
-import db from "../startup/geoDb.js";
+import type { GeoDb } from "@hebcal/geo-sqlite";
+import { getLocationDb } from "../startup/geoDb.js";
 
 export default async function (geoNameId: number) {
-  return await db?.lookupGeoname(geoNameId);
+  const db = getLocationDb() as GeoDb;
+  return await db.lookupGeoname(geoNameId);
 }
