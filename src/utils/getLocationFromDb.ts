fd336cde4d6cfd5c41fbb0b0d4874948cbe6a686
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-06-22 23:13:33 +0300

    fixed property mismatches. seems to be working

--- src/utils/getLocationFromDb.ts
+++ src/utils/getLocationFromDb.ts
@@ -5,26 +5,20 @@ import { Location } from "@hebcal/core/dist/esm/location";
 export default async function getLocationFromDb(geoNameId: number) {
   const db = getLocationDb() as GeoDb;
   const location = await db.lookupGeoname(geoNameId);
-  console.log(location);
   return location;
 }
 
 export async function getLocationFromId(geoNameId: number) {
-  console.log("function called");
-
   const loc = await getLocationFromDb(geoNameId);
   const isIsrael = loc.cc === "IL";
-  console.log(
-    `lat: ${loc.lat}. long: ${loc.long}, isIsrael: ${isIsrael}, tzid: ${loc.tzid}`
-  );
 
   return new Location(
     loc.latitude,
     loc.longitude,
     isIsrael,
-    loc.tzid,
+    loc.timeZoneId,
     loc.name,
     loc.cc,
-    loc.geoNameId
+    loc.geonameid
   );
 }
