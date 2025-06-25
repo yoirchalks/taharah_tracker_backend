a88b31a2265349c4a47f6787bf9164e5e5f3f2f1
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-06-25 09:31:22 +0300

    create and export que to send otp

---
+++ src/queues/email.que.ts
@@ -0,0 +1,12 @@
+import { Queue } from "bullmq";
+import dotenv from "dotenv";
+
+import { Redis } from "ioredis";
+dotenv.config();
+
+const connection = new Redis(process.env.REDIS_URL!, {
+  maxRetriesPerRequest: null,
+  tls: {},
+});
+
+export const emailQueue = new Queue("send_otp", { connection });
