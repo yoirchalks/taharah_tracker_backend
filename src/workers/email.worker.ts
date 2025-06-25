c0a93d596128646a83f8e9777e0a59a87fde37a1
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-06-25 09:29:59 +0300

    seperated logic for different email jobs. not yet tested

--- src/workers/email.worker.ts
+++ src/workers/email.worker.ts
@@ -1,7 +1,34 @@
-import { createClient } from "redis";
+import { Redis } from "ioredis";
+import { Worker } from "bullmq";
 import dotenv from "dotenv";
+import sendEmail from "../utils/sendEmail.js";
 
 dotenv.config();
 
-const redisClient = createClient({ url: process.env.REDIS_URL });
-redisClient.connect();
+const connection = new Redis(process.env.REDIS_URL!, {
+  maxRetriesPerRequest: null,
+  tls: {},
+});
+
+connection.on("error", (err: any) => {
+  console.log(err);
+});
+
+const emailWorker = new Worker(
+  "taharah_tracker_email",
+  async (job) => {
+    if (job.name === "send_otp") {
+      const { address, OTP } = job.data;
+      sendEmail(address, OTP);
+    }
+  },
+  { connection, removeOnComplete: { age: 60 * 20, count: 5 } }
+);
+
+emailWorker.on("completed", (job) => {
+  console.log(`job ${job.id} completed`);
+});
+
+emailWorker.on("failed", (job, err) => {
+  console.log(`job ${job?.id} failed`, err.message);
+});
