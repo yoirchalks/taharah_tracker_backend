258ccfa9a301f504243c02b24253ab40e94fd297
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-06-28 23:15:35 +0300

    added cors pkg to allow access to angular frontend

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,17 @@ import routes from "./startup/routes.js";
 import cookieParser from "cookie-parser";
 import errorHandler from "./middlewares/errorHandler.js";
 import { initGeoDb } from "./startup/geoDb.js";
+import cors from "cors";
 
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+    credentials: true,
+  })
+);
 initGeoDb();
 
 routes(app);
