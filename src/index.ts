e018323efb8ea82069c4fa317c32957002391ea6
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-07-14 07:18:23 +0300

    changed cors origin for render

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:4200",
+    origin: "https://taharah-tracker-frontend.onrender.com",
     credentials: true,
   })
 );
